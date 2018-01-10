54f8684bfd64dbe8bf3a6eccc915a2dec3892c63
MagalyMJ <magaly.m22j8@gmail.com>
2018-01-10 17:10:53 -0600

    Change to haml, create and show functionality, basic show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,22 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_parameters)
-    @user.save
+    if @user.save
+      flash[:notice] = "Post successfully created"
+    else
+      flash[:notice] = "Error"
+    end
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_parameters
     params.require(:user).permit(:email, :username, :password_digest)
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get  'users/new', to: 'users#new', as: 'new_user'
   post 'users/create', to: 'users#create', as: 'create_user'
+  get 'users/:id', to: 'users#show', as: 'user'
 end
