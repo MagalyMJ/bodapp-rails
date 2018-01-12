62861e7f270d15707ebe8404468c5e827003fe55
MagalyMJ <magaly.m22j8@gmail.com>
2018-01-12 17:56:53 -0600

    Create wedding table structure

--- db/migrate/20180111003133_create_weddings.rb
+++ db/migrate/20180111003133_create_weddings.rb
@@ -1,5 +1,13 @@
 class CreateWeddings < ActiveRecord::Migration[5.1]
   def change
+    create_table :weddings do |t|
+      t.datetime :wedding_date
+      t.string :first_partner
+      t.string :second_partner
+      t.references :user, index: true, foreing_key: true
+
+      t.timestamps
+    end
   end
 end
 
