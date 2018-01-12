09745dd873bc6526d78cf4cf98a659f629571eb6
MagalyMJ <magaly.m22j8@gmail.com>
2018-01-12 17:59:56 -0600

    Create route to new wedding view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+	#users
   get  'users/new', to: 'users#new', as: 'new_user'
   post 'users/create', to: 'users#create', as: 'create_user'
   get 'users/:id', to: 'users#show', as: 'user'
+
+  #weddings
+  get 'weddings/new', to: 'weddings#new', as: 'new_wedding'
+  post 'weddings/create', to: 'weddings#create', as: 'create_wedding'
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208040952) do
+ActiveRecord::Schema.define(version: 20180111003133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20171208040952) do
     t.string "email"
     t.string "username"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "weddings", force: :cascade do |t|
+    t.integer "userid"
+    t.datetime "wedding_date"
+    t.string "first_couple"
+    t.string "second_couple"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
